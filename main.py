4006cf2d9dba0dc29c72f29cd4c8fdf8bca58844
terminalcrazy1 <terminalcrazy1@gmail.com>
2026-06-08 15:36:09 -0400

    stuff

--- main.py
+++ main.py
@@ -13,37 +13,21 @@ from PyQt6.QtCore import Qt
 
 app = QApplication([])
 
-class MainWindow(QWidget):
-    def grid(self):
-        node_lst = []
-        for _ in range(0,30):
-            break_con = False
-            while break_con != True:
-                new_node_coords = (randint(15, 485), randint(15, 405))
-                accept_cnt = 0
-                for old_node_coords in node_lst:
-                    if ((new_node_coords[0]-old_node_coords[0])**2 + (new_node_coords[1]-old_node_coords[1])**2)**0.5 >= 25:
-                        accept_cnt += 1
-                if accept_cnt == len(node_lst):
-                    node_lst.append(new_node_coords)
-                    break_con = True
-        print(node_lst)
-        return node_lst
-
+class ClickableScene(QGraphicsScene):
     def __init__(self):
         super().__init__()
-        node_lst = self.grid()
+
+    def mousePressEvent(self, event):
+        super().mousePressEvent(event)
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle("The Earth Is Flat")
         self.setFixedSize(800, 450)
 
-        scene = QGraphicsScene(0, 0, 500, 420)
-
-        for coords in node_lst:
-            ellipsis = QGraphicsEllipseItem(0, 0, 12.5, 12.5)
-            ellipsis.setPos(coords[0], coords[1])
-            brush = QBrush(Qt.GlobalColor.blue)
-            ellipsis.setBrush(brush)
-            scene.addItem(ellipsis)
+        scene = ClickableScene()
 
         """
         column width (125px)
