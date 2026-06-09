5fa6f600ddce3ada0d2b0796e8ce6f58108ee699
terminalcrazy1 <terminalcrazy1@gmail.com>
2026-06-09 07:45:47 -0400

    add buttons + make scene clickable

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ from PyQt6.QtWidgets import (
     QGraphicsView,
     QGridLayout,
     QVBoxLayout,
+    QPushButton,
     QWidget
 )
 
 app = QApplication([])
+
+class ClickableScene(QGraphicsScene):
+    def init(self, args):
+        super().__init__(args)
+    
+    def mousePressEvent(self, event):
+        print(str(event.scenePos()))
+    
 
 class MainWindow(QWidget):
     def addDots(self, scene: QGraphicsScene):
@@ -17,13 +26,20 @@ class MainWindow(QWidget):
                 xmod = x * 40 + randint(-10,10)
                 ymod = y * 40 + randint(-10,10)
                 scene.addEllipse(xmod, ymod, 10, 10)
+    
+    def resetScene(self, scene: QGraphicsScene):
+        scene.clear()
+        self.addDots(scene)
+
+    def releaseWave(self):
+        pass
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("The Earth Is Flat")
         self.setFixedSize(800, 450)
 
-        scene = QGraphicsScene(0, 0, 500, 420)
+        scene = ClickableScene(0, 0, 500, 420)
         self.addDots(scene)
 
         """
@@ -43,8 +59,15 @@ class MainWindow(QWidget):
 
         layout.addWidget(stack, 0, 4, 2, 4)
         layout.addWidget(view, 0, 0, 4, 4)
+        
+        release_button = QPushButton("Release Wave")
+        reset_button = QPushButton("Reset")
+        release_button.clicked.connect(self.releaseWave)
+        reset_button.clicked.connect(lambda: self.resetScene(scene))
 
-        # add stack widgets here
+        stack_layout.addWidget(release_button)
+        stack_layout.addWidget(reset_button)
+        stack_layout.addStretch() # top align widgets
 
 
 
