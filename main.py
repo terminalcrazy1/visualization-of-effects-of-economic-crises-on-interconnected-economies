7f50df6f2b92fdba4fac26a37bd5a96a093f943c
terminalcrazy1 <terminalcrazy1@gmail.com>
2026-06-09 13:12:34 -0400

    refactor mouse event handling and clean up functions

--- main.py
+++ main.py
@@ -1,62 +1,22 @@
 from random import randint
 from PyQt6.QtWidgets import (
-    QApplication,
-    QGraphicsScene,
-    QGraphicsView,
-    QGridLayout,
-    QVBoxLayout,
-    QPushButton,
-    QLabel,
-    QWidget
+    QApplication, QGraphicsScene, QGraphicsView, QGridLayout,
+    QVBoxLayout, QPushButton, QLabel, QWidget
 )
 from PyQt6.QtCore import Qt
 
-app = QApplication([])
-origin = 0
-
-def onMiddleClick(event):
-    for pt in window.pts:
-        if abs(pt.coords[0] - event.scenePos().x()) < 10 and abs(pt.coords[1] - event.scenePos().y()) < 10:
-            window.load_indicator.setText("Load: " + str(pt.load))
-            print(pt.links)
-            return
-
-def onRightClick(event):
-        ct = 0
-        for pt in window.pts:
-            ct += 1
-            if abs(pt.coords[0] - event.scenePos().x()) < 10 and abs(pt.coords[1] - event.scenePos().y()) < 10:
-                return ct - 1
-
-def onLeftClick(event, last_point, scene):
-    if last_point != None:
-        for pt in window.pts:
-            if abs(pt.coords[0] - last_point[0]) < 10 and abs(pt.coords[1] - last_point[1]) < 10:
-                for pt2 in window.pts:
-                    if abs(pt2.coords[0] - event.scenePos().x()) < 10 and abs(pt2.coords[1] - event.scenePos().y()) < 10:
-                        pt.addLink(pt2)
-                        pt2.addLink(pt)
-                        scene.addLine(pt.coords[0]+5, pt.coords[1]+5, pt2.coords[0]+5, pt2.coords[1]+5)
-                        print("Linked " + str(pt.coords) + " to " + str(pt2.coords))
-                        last_point = None
-                        break
-                break
-    else:
-        last_point = (event.scenePos().x(), event.scenePos().y())
-        print("Last point set to " + str(last_point))
-    return last_point
-
-class GridPoint():
+class GridPoint:
     def __init__(self, coords: tuple[int, int], visual):
         self.coords = coords
         self.load = 0
         self.links = []
         self.visual = visual
 
-    def addLink(self, link: GridPoint):
+    def addLink(self, link: 'GridPoint'):
         self.links.append(link)
     
-    def addLoad(self, load: int, previous = []):
+    def addLoad(self, load: int, previous=None):
+        if previous is None: previous = []
         ct = 0
         for link in self.links:
             if link not in previous:
@@ -70,50 +30,51 @@ class GridPoint():
             self.visual.setBrush(Qt.GlobalColor.red)
 
 class ClickableScene(QGraphicsScene):
-    def __init__(self, *args):
+    def __init__(self, parent_window, *args):
         super().__init__(*args)
+        self.window = parent_window
         self.last_point = None
     
+    def get_pt_at(self, event):
+        pos = event.scenePos()
+        for pt in self.window.pts:
+            if abs(pt.coords[0] - pos.x()) < 10 and abs(pt.coords[1] - pos.y()) < 10:
+                return pt
+        return None
+
     def mousePressEvent(self, event):
-        global origin
+        pt = self.get_pt_at(event)
+        if not pt: return
+
         if event.button() == Qt.MouseButton.LeftButton:
-            self.last_point = onLeftClick(event, self.last_point, self)
+            if self.last_point:
+                pt2 = pt
+                pt1 = self.last_point
+                pt1.addLink(pt2)
+                pt2.addLink(pt1)
+                self.addLine(pt1.coords[0]+5, pt1.coords[1]+5, pt2.coords[0]+5, pt2.coords[1]+5)
+                self.last_point = None
+            else:
+                self.last_point = pt
+                
         elif event.button() == Qt.MouseButton.MiddleButton:
-            onMiddleClick(event)
+            self.window.load_indicator.setText("Load: " + str(pt.load))
+            
         elif event.button() == Qt.MouseButton.RightButton:
-            origin = onRightClick(event)
+            self.window.origin = self.window.pts.index(pt)
 
 class MainWindow(QWidget):
-    pts: list[GridPoint] = []
-    load_indicator: QLabel
-
-    def addDots(self, scene: QGraphicsScene):
-        for x in range(1, 12):
-            for y in range(1, 10):
-                xmod = x * 40 + randint(-10, 10)
-                ymod = y * 40 + randint(-10, 10)
-                visual =scene.addEllipse(xmod, ymod, 10, 10)
-                pt = GridPoint((xmod, ymod), visual)
-                self.pts.append(pt)
-    
-    def resetScene(self, scene: QGraphicsScene):
-        scene.clear()
-        self.pts = []
-        self.addDots(scene)
-
-    def releaseWave(self):
-        self.pts[origin].addLoad(50) 
-
     def __init__(self):
         super().__init__()
+        self.pts = []
+        self.origin = 0
         self.setWindowTitle("The Earth Is Flat")
         self.setFixedSize(800, 450)
 
-        scene = ClickableScene(0, 0, 500, 420)
-        self.addDots(scene)
+        self.scene = ClickableScene(self, 0, 0, 500, 420)
+        self.addDots(self.scene)
 
-        view = QGraphicsView(scene)
-
+        view = QGraphicsView(self.scene)
         layout = QGridLayout()
         self.setLayout(layout)
 
@@ -126,17 +87,33 @@ class MainWindow(QWidget):
         
         release_button = QPushButton("Release Wave")
         reset_button = QPushButton("Reset")
-        
         self.load_indicator = QLabel("Load: ")
         
         release_button.clicked.connect(self.releaseWave)
-        reset_button.clicked.connect(lambda: self.resetScene(scene))
+        reset_button.clicked.connect(lambda: self.resetScene(self.scene))
 
         stack_layout.addWidget(release_button)
         stack_layout.addWidget(reset_button)
         stack_layout.addWidget(self.load_indicator)
         stack_layout.addStretch()
 
+    def addDots(self, scene):
+        for x in range(1, 12):
+            for y in range(1, 10):
+                xmod = x * 40 + randint(-10, 10)
+                ymod = y * 40 + randint(-10, 10)
+                visual = scene.addEllipse(xmod, ymod, 10, 10)
+                self.pts.append(GridPoint((xmod, ymod), visual))
+    
+    def resetScene(self, scene):
+        scene.clear()
+        self.pts = []
+        self.addDots(scene)
+
+    def releaseWave(self):
+        self.pts[self.origin].addLoad(50) 
+
+app = QApplication([])
 window = MainWindow()
 window.show()
 app.exec()
