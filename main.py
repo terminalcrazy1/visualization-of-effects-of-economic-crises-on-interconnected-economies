224b91dd003a0ab0cf77297d0612c2aaabbff2fe
terminalcrazy1 <terminalcrazy1@gmail.com>
2026-06-09 13:00:40 -0400

    finish more stuff

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt6.QtWidgets import (
     QGridLayout,
     QVBoxLayout,
     QPushButton,
+    QLabel,
     QWidget
 )
 from PyQt6.QtCore import Qt
@@ -16,7 +17,7 @@ origin = 0
 def onMiddleClick(event):
     for pt in window.pts:
         if abs(pt.coords[0] - event.scenePos().x()) < 10 and abs(pt.coords[1] - event.scenePos().y()) < 10:
-            print("Clicked on point at " + str(pt.coords) + " with load " + str(pt.load) + " and " + str(len(pt.links)) + " links")
+            window.load_indicator.setText("Load: " + str(pt.load))
             print(pt.links)
             return
 
@@ -82,6 +83,7 @@ class ClickableScene(QGraphicsScene):
 
 class MainWindow(QWidget):
     pts: list[GridPoint] = []
+    load_indicator: QLabel
 
     def addDots(self, scene: QGraphicsScene):
         for x in range(1, 12):
@@ -122,11 +124,15 @@ class MainWindow(QWidget):
         
         release_button = QPushButton("Release Wave")
         reset_button = QPushButton("Reset")
+        
+        self.load_indicator = QLabel("Load: ")
+        
         release_button.clicked.connect(self.releaseWave)
         reset_button.clicked.connect(lambda: self.resetScene(scene))
 
         stack_layout.addWidget(release_button)
         stack_layout.addWidget(reset_button)
+        stack_layout.addWidget(self.load_indicator)
         stack_layout.addStretch()
 
 window = MainWindow()
